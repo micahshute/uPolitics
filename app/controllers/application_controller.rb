1bbee47acc67b7a8e3f254b7458149547795e398
Micah Shute <micah.shute@gmail.com>
2018-08-18 14:41:47 -0500

    initial file and envorionment setup complete

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < Sinatra::Base
+
+    configure do 
+        set :public_folder, 'public'
+        set :views, 'app/views'
+        enable :sessions
+        set :session_secret, ENV.fetch('SESSION_SECRET'){
+            # SecureRandom.hex(64)
+            "SAVE_OUTPUT_OF_ABOVE_AS_ENV_VARIABLE"
+        }
+    end
+
+end
