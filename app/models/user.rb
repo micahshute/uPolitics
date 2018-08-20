0f703855d27d0a06264155c623f0780c1061c88c
micahshute <micah.shute@gmail.com>
2018-08-19 22:01:34 -0500

    completed authenticate views, changed email to username in db

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,20 @@
 class User < ActiveRecord::Base
     has_secure_password
     validates :name, presence: true
-    validates :email, presence: true
+    validates :username, presence: true
     validates_uniqueness_of :email
 
     belongs_to :state
     has_many :posts
     has_many :reactions
 
-    has_many :user_bills 
+    has_many :user_bills
     has_many :followed_bills, :through => :user_bills, :source => :bill
     has_many :user_members
     has_many :followed_members, :through => :user_members, :source => :members
 
     has_many :reacted_bills, :through => :reactions, :source => :reactable, source_type: 'Bill'
     has_many :reacted_members, :through => :reactions, :source => :reactable, source_type: 'Member'
-    has_many :reacted_posts, :through => :reactions, :source => :reactable, source_type: 'Post' 
-    
+    has_many :reacted_posts, :through => :reactions, :source => :reactable, source_type: 'Post'
+
 end
