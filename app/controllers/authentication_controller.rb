c374eb5cb8a8f45002893bbb02d26332b7875519
Micah Shute <micah.shute@gmail.com>
2018-08-20 08:27:09 -0500

    added map, inserted map links

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -10,6 +10,21 @@ class AuthenticationController < ApplicationController
         erb :"authenticate/signup"
     end
 
+    get '/profile' do
+        authorize
+        erb :"users/profile"
+    end
+
+    get '/profile/:state' do
+        state_info = params[:state].split('_')
+        @state_hash = {
+            name: state_info[0].split('-').join(' '),
+            abbreviation: state_info[1]
+        }
+        
+        "Show state details"
+    end
+
     post '/login' do
         redirect '/' if authorized?
         if ((user = User.find_by(username: params[:username])) && user.authenticate(params[:password]))
@@ -26,10 +41,19 @@ class AuthenticationController < ApplicationController
         user = User.new(params[:user])
         if user.save
           session[:user_id] = user.id
-          redirect '/'
+          redirect '/profile'
         else
           @errors = user.errors.messages
           erb :"/authenticate/signup"
         end
     end
+
+    post '/profile/:state' do
+        authorize
+        state = State.new(params[:state])
+        current_user.state = state
+        current_user.save
+        redirect '/'
+    end
+    
 end
