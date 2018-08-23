c273054310deb08bfc1a40760743ee8503f732c0
Micah Shute <micah.shute@gmail.com>
2018-08-23 16:27:31 -0500

    Polished member_controller and member_card - took out photos for load time

--- app/controllers/member_controller.rb
+++ app/controllers/member_controller.rb
@@ -3,11 +3,11 @@ class MemberController < ApplicationController
     get '/members/senate' do
         @pageinfo = {
             topic: "Current Senators",
-            content: "Organized by name (more organization styles coming soon)"
+            content: "Organized by state (more organization styles coming soon)"
         }
       
         @members = MemberAPI.all_senators
-
+        @members.sort!{|a,b| a.state <=> b.state}
         erb :browse do 
             erb :"members/senator_card"
         end
