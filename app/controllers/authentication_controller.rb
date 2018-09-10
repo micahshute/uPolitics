d56f1275e67dc50ea780202e65e1eb895cd95ab8
micah <micah.shute@gmail.com>
2018-09-10 15:24:55 -0500

    testing site down

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -28,7 +28,11 @@ class AuthenticationController < ApplicationController
         user = current_user
         user.state = state
         @user = UserAPIDecorator.new(user: user)
-        erb :"authenticate/verify_setup"
+        begin
+            erb :"authenticate/verify_setup"
+        rescue
+            erb :errors/site_down"
+        end
     end
 
     post '/login' do
